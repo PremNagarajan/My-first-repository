6b771b7bb9208e0757b92a88d7cd5c9d0f822640
Prem Nagarajan <prem.cmnagar@gmail.com>
2016-11-09 07:25:18 +0000

    Find the longest palindrome substring

--- Strings/manacher_algo_longest_palindrome_substring.py
+++ Strings/manacher_algo_longest_palindrome_substring.py
@@ -4,6 +4,49 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
+        def preProcess(s):
+            if not s:
+                return ['^', '$']
+            T = ['^']
+            for c in s:
+                T +=  ['#', c]
+            T += ['#', '$']
+            return T
+
+        T = preProcess(s)
+        P = [0] * len(T) 
+        center, right = 0, 0
+        for i in xrange(1, len(T) - 1):
+            i_mirror = 2 * center - i
+            if right > i:
+                P[i] = min(right - i, P[i_mirror])
+            else:
+                P[i] = 0
+
+            while T[i + 1 + P[i]] == T[i - 1 - P[i]]:
+                P[i] += 1
+
+            if i + P[i] > right:
+                center, right = i, i + P[i]       
+        
+        max_i = 0
+        for i in xrange(1, len(T) - 1):
+            if P[i] > P[max_i]:
+                max_i = i
+        start = (max_i - 1 - P[max_i]) / 2
+        return s[start : start + P[max_i]]
+
+s = Solution()
+string = "aa"
+print(s.longestPalindrome(string))
+
+'''
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
         length = len(s)
         if length:
             max_len = 1
@@ -39,8 +82,4 @@ class Solution(object):
             i = i + 1
 
         return max_string.replace('#', '')
-
-s = Solution()
-string = "aa"
-print(s.longestPalindrome(string))
-
+'''
